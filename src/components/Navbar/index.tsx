8f36a904d8484bbefbd11c3ed06f030f14bebee6
Adnan Ali <adnanali6688@gmail.com>
2023-02-17 00:41:45 +0100

    created header

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,5 @@
 import styles from "./Navbar.module.scss";
+import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 
 type Props = {};
 
@@ -7,9 +8,24 @@ const Navbar = (props: Props) => {
     <div className={styles.container}>
       <div className={styles.logo}>LOGO</div>
       <div className={styles.socialMedia}>
-        <div>Gmail</div>
-        <div>GitHub</div>
-        <div>LinkedIn</div>
+        <div className={styles.iconBox}>
+          <a className={styles.link} href="mailto:adnanali6688@gmail.com">
+            {<FaEnvelope className={styles.icon} />}
+          </a>
+        </div>
+        <div className={styles.iconBox}>
+          <a
+            className={styles.link}
+            href="https://www.linkedin.com/in/adnan-ali-5b9736111/"
+          >
+            {<FaLinkedin className={styles.icon} />}
+          </a>
+        </div>
+        <div className={styles.iconBox}>
+          <a className={styles.link} href="https://github.com/adnan725">
+            {<FaGithub className={styles.icon} />}
+          </a>
+        </div>
       </div>
     </div>
   );
