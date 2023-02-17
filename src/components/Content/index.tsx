591bff08bed99e3cb4f6d23e9718cd554c3e35d8
Adnan Ali <adnanali6688@gmail.com>
2023-02-17 12:55:17 +0100

    implemented content box

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -5,9 +5,25 @@ type Props = {};
 const Content = (props: Props) => {
   return (
     <div className={styles.container}>
-      <div className={styles.descriptionWrapper}>Left</div>
+      <div className={styles.descriptionWrapper}>
+        <div>
+          <p className={styles.heading}>Heading</p>
+          <div className={styles.content}>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Molestias necessitatibus architecto laudantium numquam magni
+              incidunt vel nulla laboriosam maiores? Molestiae qui recusandae
+              debitis aut ipsa! Quisquam nam nobis corrupti iste. Lorem, ipsum
+              dolor sit amet consectetur adipisicing elit. Molestias
+              necessitatibus architecto laudantium numquam magni incidunt vel
+              nulla laboriosam maiores? Molestiae qui recusandae debitis aut
+              ipsa! Quisquam nam nobis corrupti iste?
+            </p>
+          </div>
+        </div>
+      </div>
       <div className={styles.linksWrapper}>
-        <p className={styles.projectsHeading}>Projects</p>
+        <p className={styles.projectsHeading}>Credentials</p>
         <div className={styles.links}>
           <p
             className={`${styles.link} ${styles.borderGredient} 
