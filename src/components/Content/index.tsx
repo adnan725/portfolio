5205feca0b1f23b835abfdac6d70ab84fa15dccc
Adnan Ali <adnanali6688@gmail.com>
2023-02-18 02:26:52 +0100

    implemented active link functionality

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,24 +1,47 @@
 import styles from "./Content.module.scss";
+import React, { useState } from "react";
 
 type Props = {};
 
 const Content = (props: Props) => {
+  const [activeLink, setActiveLink] = useState("about");
+
+  const linkHandler = (link: string) => {
+    setActiveLink(link);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.descriptionWrapper}>
         <div>
           <p className={styles.heading}>Heading</p>
           <div className={styles.content}>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Molestias necessitatibus architecto laudantium numquam magni
-              incidunt vel nulla laboriosam maiores? Molestiae qui recusandae
-              debitis aut ipsa! Quisquam nam nobis corrupti iste. Lorem, ipsum
-              dolor sit amet consectetur adipisicing elit. Molestias
-              necessitatibus architecto laudantium numquam magni incidunt vel
-              nulla laboriosam maiores? Molestiae qui recusandae debitis aut
-              ipsa! Quisquam nam nobis corrupti iste?
-            </p>
+            {activeLink === "about" && (
+              <p className={styles.paragraph}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Molestias necessitatibus architecto laudantium numquam magni
+                incidunt vel nulla laboriosam maiores? Molestiae qui recusandae
+                debitis aut ipsa! Quisquam nam nobis corrupti iste.
+              </p>
+            )}
+            {activeLink === "skills" && (
+              <p className={styles.paragraph}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Molestias necessitatibus architecto laudantium numquam magni
+                incidunt vel nulla laboriosam maiore
+              </p>
+            )}
+            {activeLink === "experience" && (
+              <p className={styles.paragraph}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Molestias necessitatibus architecto laudantium numquam magni
+                incidunt vel nulla laboriosam maiores? Molestiae qui recusandae
+                debitis aut ipsa! Quisquam nam nobis corrupti iste. us
+                architecto laudantium numquam magni incidunt vel nulla
+                laboriosam maiores? Molestiae qui recusandae debitis aut ipsa!
+                Quisquam nam nobis corrupti iste.
+              </p>
+            )}
           </div>
         </div>
       </div>
@@ -26,28 +49,25 @@ const Content = (props: Props) => {
         <p className={styles.projectsHeading}>Credentials</p>
         <div className={styles.links}>
           <p
+            onClick={() => linkHandler("about")}
             className={`${styles.link} ${styles.borderGredient} 
             ${styles.borderColor}`}
           >
             About
           </p>
           <p
+            onClick={() => linkHandler("skills")}
             className={`${styles.link} ${styles.borderGredient} 
             ${styles.borderColor}`}
           >
             Skills
           </p>
           <p
+            onClick={() => linkHandler("experience")}
             className={`${styles.link} ${styles.borderGredient} 
             ${styles.borderColor}`}
           >
             Experience
-          </p>
-          <p
-            className={`${styles.link} ${styles.borderGredient} 
-            ${styles.borderColor}`}
-          >
-            Projects
           </p>
         </div>
       </div>
